90605109831853062529196a711fadbbe2c3e0ff
JOE NASR <joenasr@gmail.com>
2026-04-04 20:14:24 +0300

    Add pricing page

---
+++ app/pricing/page.js
@@ -0,0 +1,15 @@
+export default function Pricing() {
+  return (
+    <main>
+      <h1>Pricing</h1>
+      <div>
+        <h2>Monthly</h2>
+        <p>49 AED / month</p>
+      </div>
+      <div>
+        <h2>Yearly</h2>
+        <p>399 AED / year</p>
+      </div>
+    </main>
+  )
+}
